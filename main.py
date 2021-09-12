2c2bc2413295c1feb6e3f5bf1decec9f88320f3b
Krzysiek <szlauerdev@gmail.com>
2021-09-12 11:43:47 +0200

    Ascii art and looping added

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 # a program that takes any String input and converts it into Morse Code
 from codes_data import codes_dict
+from ascii_art import logo
 
-my_string = input("Type the string that you want to convert\n")
 
-encoded_string = ""
-for symbol in my_string.upper():
-    if symbol == " ":
-        encoded_string += "/ "
-    else:
-        encoded_string += codes_dict[symbol] + " "
+def convert_text():
+    my_string = input("Type the string that you want to convert\n>>>")
 
-print(encoded_string)
-print(codes_dict['"'])
+    encoded_string = ""
+    for symbol in my_string.upper():
+        if symbol == " ":
+            encoded_string += "/ "
+        else:
+            encoded_string += codes_dict[symbol] + " "
+
+    print(encoded_string)
+
+def check_another():
+    answer = input("Do you want to convert another sentence? (Y/N)\n>>>")
+    return True if answer.upper() == "Y" else False
+
+print(logo + "\n")
+run_program = True
+while run_program:
+    convert_text()
+    run_program = check_another()
